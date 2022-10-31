dd0136ea0f8fc58c389cbd5879e1f1e9b1438952
jhs38906 <kyjj13690@naver.com>
2022-10-31 13:37:53 +0900

    10.30 기능 업데이트
    내부 UI구조에 대한 수정, 카메라를 통한 스캔, 저장기능 추가 (OCR기술 구현하였으나 카메라와의 연동과정에 대한 보완 필요)

--- MyApplication/app/src/main/java/com/example/myapplication/mainactivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/mainactivity2.java
@@ -2,10 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
 
 public class mainactivity2 extends AppCompatActivity {
 
@@ -18,13 +23,14 @@ public class mainactivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainactivity2);
+//        TextView totalcal = findViewById(R.id.칼로리입력칸);
+
 
         movebutton = (Button)findViewById(R.id.mvbtn5);
-
         movebutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), mainactivity5.class);
+                Intent intent = new Intent(getApplicationContext(), jadong.class);
                 startActivity(intent);
             }
         });
@@ -34,7 +40,7 @@ public class mainactivity2 extends AppCompatActivity {
         movebutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), mainactivity6.class);
+                Intent intent = new Intent(getApplicationContext(), sudong.class);
                 startActivity(intent);
             }
         });
--- MyApplication/app/src/main/java/com/example/myapplication/mainactivity3.java
+++ MyApplication/app/src/main/java/com/example/myapplication/mainactivity3.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myapplication.R;
+
 public class mainactivity3 extends AppCompatActivity {
 
     @Override
