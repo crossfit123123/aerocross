0e0689976076e8d63d753555b47333598f7e647c
jhs38906 <kyjj13690@naver.com>
2022-11-06 21:56:58 +0900

    11.06 update
    11.06 레이아웃,gnu수정

---
+++ myapplication/app/src/main/java/com/example/myapplication/startactivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class startactivity extends AppCompatActivity {
+
+    TextView t1,t2,t3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startactivity);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(startactivity.this, MainActivity1.class);
+                startActivity(intent);
+
+            }
+        }, 1000);
+    }
+}
